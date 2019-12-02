3fdef7856bc300e1d4e387ffddacffa18f03bc56
anjana6 <anjanashakthi95@gmail.com>
2019-12-02 10:36:42 +0530

    complete form app

--- LoginForm/formapp/lib/ui/Login.dart
+++ LoginForm/formapp/lib/ui/Login.dart
@@ -13,6 +13,25 @@ class LoginState extends State<Login>{
 
   final TextEditingController _userController = new TextEditingController();
   TextEditingController _pswdController = new TextEditingController();
+  String _welcomeString = "";
+
+  void _erase(){
+    setState(() {
+      _userController.clear();
+      _pswdController.clear();
+    });
+  }
+
+  void _showWelcome(){
+    setState(() {
+      if(_userController.text.isNotEmpty && _pswdController.text.isNotEmpty){
+        _welcomeString = _userController.text;
+      }else{
+        _welcomeString = "Nothing";
+      }
+    }); 
+
+  }
   @override
   Widget build(BuildContext context) {
     
@@ -35,7 +54,7 @@ class LoginState extends State<Login>{
             ),
 
             new Container(
-              height: 180.0,
+              height: 180.0, 
               width: 380.0,
               color: Colors.white,
               child: new Column(
@@ -52,11 +71,43 @@ class LoginState extends State<Login>{
                     controller: _pswdController,
                     decoration: new InputDecoration(
                       hintText: 'password',
-                      icon: new Icon(Icons.person)
+                      icon: new Icon(Icons.lock)
+                    ),
+                    obscureText: true,
+                  ),
+                  new Padding(padding: new EdgeInsets.all(15.5),),
+                  new Center(
+                    child: new Row(
+                      children: <Widget>[
+                        new Container(
+                          margin: const EdgeInsets.only(left:38.0),
+                         child:new RaisedButton(
+                          onPressed: _showWelcome,
+                          color: Colors.redAccent ,
+                         child: new Text("Login", style: new TextStyle(color: Colors.white,fontSize: 16.9),),
+                        )
+                        ),
+
+                         new Container(
+                          margin: const EdgeInsets.only(left:78.0),
+                         child:new RaisedButton(
+                          onPressed: _erase,
+                          color: Colors.redAccent ,
+                         child: new Text("Clear", style: new TextStyle(color: Colors.white,fontSize: 16.9),),
+                        ),
+                        )
+                      ],
                     ),
                   )
                 ],
               ),
+            ),
+            new Padding(padding: new EdgeInsets.all(14.0),),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text("welcome, $_welcomeString",style: TextStyle(color: Colors.white,fontSize: 19.4,fontWeight: FontWeight.w500),),
+              ],
             )
           ],
         ),
