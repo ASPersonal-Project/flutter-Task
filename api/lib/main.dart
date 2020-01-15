e7f7333d3b3ebe7fab98660d040be95e6b7a44e1
anjana6 <anjanashakthi95@gmail.com>
2020-01-15 23:03:53 +0530

    api

---
+++ api/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new MaterialApp(
+    home: new Scaffold(
+      appBar: new AppBar(
+        title: new Text('JSON Parse'),
+        centerTitle: true,
+      ),
+      body: new Center(
+        child: new Text('Parsing ....'),
+      ),
+    )
+  ));
+
+}
+  
